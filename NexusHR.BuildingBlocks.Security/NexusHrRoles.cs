a7d65d06e1d601077ed0744ba4592e7ce1aa3a4b
Atakan Bektaş <123401930+atakanbektas@users.noreply.github.com>
2026-07-31 10:09:42 +0300

    feat(security): add onboarding permissions

--- NexusHR.BuildingBlocks.Security/NexusHrRoles.cs
+++ NexusHR.BuildingBlocks.Security/NexusHrRoles.cs
@@ -1,27 +1,14 @@
-﻿namespace NexusHR.BuildingBlocks.Security;
+namespace NexusHR.BuildingBlocks.Security;
 
 public static class NexusHrRoles
 {
-    public const string HrSpecialist =
-        "HrSpecialist";
-
-    public const string HrManager =
-        "HrManager";
-
-    public const string DepartmentManager =
-        "DepartmentManager";
-
-    public const string ItSpecialist =
-        "ItSpecialist";
-
-    public const string FinanceSpecialist =
-        "FinanceSpecialist";
-
-    public const string Admin =
-        "Admin";
-
-    public const string Auditor =
-        "Auditor";
+    public const string HrSpecialist = "HrSpecialist";
+    public const string HrManager = "HrManager";
+    public const string DepartmentManager = "DepartmentManager";
+    public const string ItSpecialist = "ItSpecialist";
+    public const string FinanceSpecialist = "FinanceSpecialist";
+    public const string Admin = "Admin";
+    public const string Auditor = "Auditor";
 
     public const string CandidateReaders =
         "HrSpecialist,HrManager,DepartmentManager,Auditor";
@@ -30,8 +17,14 @@ public static class NexusHrRoles
         "HrSpecialist";
 
     public const string HiringReaders =
-    "HrSpecialist,HrManager,DepartmentManager,Auditor";
+        "HrSpecialist,HrManager,DepartmentManager,Auditor";
 
     public const string HiringEditors =
         "HrSpecialist";
+
+    public const string OnboardingReaders =
+        "HrSpecialist,HrManager,DepartmentManager,ItSpecialist,Auditor";
+
+    public const string OnboardingEditors =
+        "HrSpecialist,ItSpecialist";
 }
