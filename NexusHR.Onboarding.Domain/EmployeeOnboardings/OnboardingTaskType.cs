fe913b9cbd63ab00dceba5b49fae2fb37a627aba
Atakan Bektaş <123401930+atakanbektas@users.noreply.github.com>
2026-07-31 09:59:13 +0300

    feat(onboarding): add task type

---
+++ NexusHR.Onboarding.Domain/EmployeeOnboardings/OnboardingTaskType.cs
@@ -0,0 +1,8 @@
+namespace NexusHR.Onboarding.Domain.EmployeeOnboardings;
+
+public enum OnboardingTaskType
+{
+    EquipmentPreparation = 1,
+    CorporateAccountProvisioning = 2,
+    AccessProvisioning = 3
+}
